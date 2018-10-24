bbdc55bbadb729f11512460f136ff7ff2d9a6ccf
wjh <535944211@qq.com>
2018-10-24 17:33:15 +0800

    修改

--- src/main/java/com/lz/web/realms/UserRealm.java
+++ src/main/java/com/lz/web/realms/UserRealm.java
@@ -3,6 +3,7 @@ package com.lz.web.realms;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
@@ -27,6 +28,7 @@ public class UserRealm extends AuthorizingRealm{
 
 	@Override
 	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+		UsernamePasswordToken usernamePasswordToken = (UsernamePasswordToken) authenticationToken;
 		return null;
 	}
 }
